a35222bec5b3258eb32299f493fc0c44ed60b79f
ben wiseley <wiseleyb@dinghy.local>
2014-01-22 19:16:06 -0800

    Add contacts and users scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Twil::Application.routes.draw do
+  resources :contacts
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
